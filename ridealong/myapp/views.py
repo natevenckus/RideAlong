61bc1caef8d1bfa5b8df52fbe318f32a9af39b9b
natevenckus <nvenckus@purdue.edu>
2019-02-10 22:46:31 -0500

    Added test URL route and view for password authentication

--- ridealong/myapp/views.py
+++ ridealong/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def pwtest(request):
+   #return render(request, "myapp/template/pwtest.html", {})
+   return HttpResponse("Password test (updated)")
