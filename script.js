bd5f247e199a94f24266fb575ad751f357557dac
Richa Tenany <richa.tenany@gmail.com>
2019-02-13 17:08:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function storeCreds(){
     var username = document.getElementById("email").value;
     var password = document.getElementById("reg_password").value;
-            console.log(username, password);
+            console.log("Username: " + username);
+            console.log("Password: " + password);
     
 }
