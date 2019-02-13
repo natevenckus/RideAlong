d5a927e925814548c4565683bae505578ee38674
Richa Tenany <richa.tenany@gmail.com>
2019-02-13 02:27:26 -0500

    extra

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-var username = document.getElementsByName("email").values;
-var password = document.getElementsByName("pass").values;
 
+
+function validate(){
+    var username = document.getElementById("uniEmail").value;
+    var password = document.getElementById("password").value;
+        if(password.length <= 8){
+            alert("Please make sure your password is longer than 8 characters");
+        }
+}
