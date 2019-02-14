2b760546c7256e231aca4aa62df202cbdde2261c
Zidaan Dutta <zdutta@purdue.edu>
2019-02-14 01:26:52 -0500

    static configuration

--- ridealong/ridealong/settings.py
+++ ridealong/ridealong/settings.py
@@ -128,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#telling Django to look for static files outside of specific app
+
+STATICFILE_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
