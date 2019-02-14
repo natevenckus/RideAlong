30411e87622bf77a84b4226b82415f55d2515cb1
Zidaan Dutta <zdutta@purdue.edu>
2019-02-14 16:11:36 -0500

    rendering register and login

--- ridealong/accounts/views.py
+++ ridealong/accounts/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'homePage.html')
+
+def loginPage(request):
+    return render(request,'login.html')
+
+def registerPage(request):
+    return render(request,'register.html')
--- script.js
+++
@@ -1,7 +0,0 @@
-function storeCreds(){
-    var username = document.getElementById("email").value;
-    var password = document.getElementById("reg_password").value;
-            console.log("Username: " + username);
-            console.log("Password: " + password);
-    
-}
