dcf835fa65cdc14a9ea0b8998a25fad4230a6a5c
Zidaan Dutta <zdutta@purdue.edu>
2019-02-17 16:51:36 -0500

    added forms.py

--- ridealong/accounts/views.py
+++ ridealong/accounts/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import  UserCreationForm
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
 from django.shortcuts import render,redirect
+from accounts.forms import RegistrationForm
 
 
 def index(request):
-    formRegister = UserCreationForm()
-    formLogin = UserCreationForm()
+    formRegister = RegistrationForm()
+    formLogin = AuthenticationForm()
 
     return render(request,'homePage.html', {'formRegister':formRegister,'formLogin':formLogin })
 
 
 
 #not needed
-def loginPage(request):
+def loginpage(request):
     return render(request,'login.html')
 
 def registerPage(request):
@@ -22,5 +23,4 @@ def registerPage(request):
 
 def register(request):
     form = UserCreationForm()
-
     return render(request,'homePage.html',{'form':form})
