94b6613f76d85c8dc504b1f4c42b3a421da01c75
natevenckus <nvenckus@purdue.edu>
2019-02-21 00:14:09 -0500

    Moved models to appropriate locations

--- ridealong/driver/models.py
+++ ridealong/driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class DriveRequest(models.Model):
+	ID = models.AutoField(primary_key=True)
+	Rider = models.ForeignKey(User, on_delete=models.CASCADE)
+	FromLat = models.DecimalField(decimal_places=10, max_digits=15)
+	FromLong = models.DecimalField(decimal_places=10, max_digits=15)
+	ToLat = models.DecimalField(decimal_places=10, max_digits=15)
+	ToLong = models.DecimalField(decimal_places=10, max_digits=15)
+	MinDepartTime = models.DateTimeField()
+	MaxDepartTime = models.DateTimeField()
+	PriceOffer = models.DecimalField(decimal_places=2, max_digits=10)
+	LuggageSqFt = models.DecimalField(decimal_places=2, max_digits=5)
+	RequestTime = models.DateTimeField()
+	Completed = models.BooleanField()
+	CompleteTime = models.DateTimeField()
