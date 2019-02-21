d9fc3e05a12c71ef61dd00dbc423d2792396fb22
Zidaan Dutta <zdutta@purdue.edu>
2019-02-21 00:07:02 -0500

    rearranged driver related files

---
+++ ridealong/driver/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='driverpage'),
+    path('ridepopup',views.ridepopup,name='ridepopup')
+]
--- ridealong/ridealong/settings.py
+++ ridealong/ridealong/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'accounts'
+    'accounts',
+    'driver'
 ]
 
 MIDDLEWARE = [
