c9c61fbf26b7c00a90bc2e0b1b372887baaaf52d
Zidaan Dutta <zdutta@purdue.edu>
2019-02-22 02:49:06 -0500

    sending proper POST request

--- ridealong/driver/views.py
+++ ridealong/driver/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print(request.GET['departLoc'])
     return render(request,"driver_page.html")
+
+def storeRideInfo(request):
+    return
 
 def ridepopup(request):
     return render(request,'ridePopup.html')
