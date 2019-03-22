943b8c45862d29117dadd406fcaf07090ef30462
Zidaan Dutta <zdutta@purdue.edu>
2019-03-22 07:57:06 -0400

    search request working

--- ridealong/driver/views.py
+++ ridealong/driver/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import DriveRequest, Car
 from accounts.models import Profile
+from django.contrib.postgres.search import SearchQuery, SearchVector
+from django.db.models.functions import Cast
+from django.db.models import CharField
 from . import views
 
 # Create your views here.
@@ -63,16 +66,34 @@ def index(request):
         driveRequest_instance.save()
 
     driveRequests = DriveRequest.objects.all()
+    #times = DriveRequest.objects.all().values_list('pickupTime',flat=True)
+    #print (times)
     print("driveRequests:")
     print(driveRequests)
     return render(request,"driver_page.html",{'isIndex':True,'driveRequests':driveRequests})
 
 def driverSearch(request):
-    #ex. query http://localhost:8000/driver/search?searchText=West&filter=location
+    #ex. query http://localhost:8000/driver/search?searchLocation=West&filter=location
+    #ex. query for date http://localhost:8000/driver/search?searchYear=2222&searchMonth=2&searchDay=2&filter=date
     #filter options: location,date,price,luggage,passengershttp://localhost:8000/driver/search?Filter=Price
     if request.method == "GET":
+        print (request.GET)
         if request.GET['filter'] == 'location':
-            searchResult = DriveRequest.objects.filter(departLoc__search=request.GET['searchText'])
+            searchResult = DriveRequest.objects.filter(departLoc__search=request.GET['searchLocation'])
+        elif request.GET['filter'] == 'date':
+            searchResult = DriveRequest.objects.filter(pickupTime__year=request.GET['searchYear'], pickupTime__month=request.GET['searchMonth'],pickupTime__day=request.GET['searchDay'])
+        elif request.GET['filter'] == 'price':
+            q = SearchQuery(request.GET['searchPrice'])
+            vector = SearchVector(Cast('PriceOffer', CharField()))
+            searchResult=DriveRequest.objects.annotate(search=vector).filter(search=q)
+        elif request.GET['filter'] == 'luggage':
+            q = SearchQuery(request.GET['searchLuggage'])
+            vector = SearchVector(Cast('numOfBaggage', CharField()))
+            searchResult=DriveRequest.objects.annotate(search=vector).filter(search=q)
+        elif request.GET['filter'] == 'passenger':
+            q = SearchQuery(request.GET['searchPassenger'])
+            vector = SearchVector(Cast('numOfSeats', CharField()))
+            searchResult=DriveRequest.objects.annotate(search=vector).filter(search=q)
 
     return render(request,"driver_page.html",{'isIndex':False,'searchResult':searchResult})
 
