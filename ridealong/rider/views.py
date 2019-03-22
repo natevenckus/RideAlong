55d75bc47bc4f176bf52a6f64b13953473164dbe
natevenckus <nvenckus@purdue.edu>
2019-03-22 02:29:41 -0500

    Added authentication check to rider page

--- ridealong/rider/views.py
+++ ridealong/rider/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('loginpage')
+    
     return render(request,'rider_page.html')
