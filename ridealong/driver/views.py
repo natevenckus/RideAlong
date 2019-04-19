0957355966203fb9f71667c598f3dc6046c573ae
Richa Tenany <richa.tenany@gmail.com>
2019-04-19 04:27:47 -0400

    vish cant spell

--- ridealong/driver/views.py
+++ ridealong/driver/views.py
@@ -58,7 +58,7 @@ def index(request):
         numOfSeats = request.POST['seats']
         numOfBaggage = request.POST['baggage']
         priceOffer = request.POST['Price']
-        if destID and originID and departLoc and arrivalLoc and pickupTime and dropTime and numOfSeats and numOfBaggage:
+        if destID and originID and departLoc and arrivalLoc and pickupTime and dropTime and numOfSeats and numOfBaggage and priceOffer:
             coordinates = getGeo(originID,destID)
             print (coordinates)
             driveRequest_instance = DriveRequest.objects.create(
@@ -69,7 +69,7 @@ def index(request):
                 dropTime = request.POST['dropTime'],
                 numOfSeats = request.POST['seats'],
                 numOfBaggage = request.POST['baggage'],
-                PriceOffer = reqeust.POST['Price'],
+                PriceOffer = request.POST['Price'],
                 FromLat = coordinates[0],
                 FromLong = coordinates[1],
                 ToLat = coordinates[2],
