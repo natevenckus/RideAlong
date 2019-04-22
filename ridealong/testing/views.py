fca3026043500104d78b36c363d73f447605973a
Sougat Pradhan <pradhan1@purdue.edu>
2019-04-22 16:20:51 -0400

    Update views.py
    messing around with testing page

--- ridealong/testing/views.py
+++ ridealong/testing/views.py
@@ -3,17 +3,20 @@ from django.http import HttpResponse
 from django.test import TestCase
 from django.test import Client
 from accounts import views
+import requests
  
 def index(request):
     
     print("in loginTestCase")
     c = Client()
-    response = c.post('', {'username': 'sougz7', 'password': 'ridealong678', 'remember_me': 'true', 'login': 'Login'})
-    print (response.status_code)
-    if response.status_code == 400:
+    return HttpResponse("testing webpage")
+    r = requests.post(url='localhost:8000/', data={'username': 'sougz7', 'password': 'ridealong678', 'remember_me': 'true', 'login': 'Login'})
+    print ("response status code")
+    print(r.status_code)
+    if r.status_code == 200:
         print("passed loginTestCase")
         return HttpResponse("passed loginTestCase")
     else:
         print("failed loginTestCase")
         return HttpResponse("failed loginTestCase")
-    return HttpResponse("testing page")
+    
